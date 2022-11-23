6c520838fb199f59b4a2638c6ce4d96bf0975cfc
Aadityaa23 <aadityaa32@gmail.com>
2022-11-23 21:03:24 +1100

    Added new pages and created routes

--- cc2/src/App.js
+++ cc2/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import DAC from "./pages/DAC";
+
 function App() {
-	return <h1>Hello World</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/AboutUs" element={<AboutUs />} />
+				<Route path="/DAC" element={<DAC />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
